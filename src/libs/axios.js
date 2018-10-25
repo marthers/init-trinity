932b313402a5fa1b1264b04b62373671677fa515
marthers <nmzyg252@163.com>
2018-10-25 14:48:10 +0800

    添加请求拦截器的请求头

--- src/libs/axios.js
+++ src/libs/axios.js
@@ -43,8 +43,9 @@ class HttpRequest {
       this.queue[url] = true
       console.log('nterceptors.request__config:')
       console.log(config)
-      // config.headers['Request-Datatime '] = new Date().getTime()
-      // config.headers['Trinity-Token'] = localStorage.getItem('Trinity-Token') != null ? localStorage.getItem('Trinity-Token') : ''
+      config.headers['Request-Datatime '] = new Date().getTime()
+      config.headers['Trinity-Token']     = localStorage.getItem('Trinity-Token') != null ? localStorage.getItem('Trinity-Token') : '';
+      config.headers['Content-Type']      = 'application/json; charset=utf-8';
       return config
     }, error => {
       return Promise.reject(error)
